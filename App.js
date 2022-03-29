7d3f7faeb83b6b5d3af005b2bc6c3c291dbe242a
driazi <driaziran@gmail.com>
2022-03-29 22:21:15 +0430

    Add files via upload

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
-const url='https://dummyjson.com/products/1';
-const [data, setData] = useState({})
 
-useEffect(() => {
-  fetch(url)
-  .then(res => res.json())
-  .then(res => setData(res))
-  .catch(error => console.log(error.message))
-} , [])
+   const styleObj = {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: "#4a54f1",
+    textAlign: "center",
+    paddingTop: "50px",
+}
 
+  const url = 'https://dummyjson.com/products/1';
+  const [data , setData] = useState({})
+
+
+  useEffect(()=>{
+    fetch(url)
+    .then(res => res.json())
+    .then(res => setData(res))
+    .catch(error=> console.log(error.message))
+  } , [])
 
 useEffect(()=>{
   console.log(data);
 } , [data])
 
-
   return (
-      <div className="App">
-        <div>
-         <p style={{ color: "red" }}> title:  {data.title}</p>
-         <p style={{ color: "blue" }}> Id : {data.id}</p>
-        </div>
+    <div className="App">
+      <div>
+        <p style={styleObj}>id : {data.id}</p>
+        <p style={styleObj}>title : {data.title}</p>
+        <p style={styleObj}>description : {data.description}</p>
+        <p style={styleObj}>category : {data.category}</p>
+        {/* <p>images : {data.images[0]}</p> */}
       </div>
+    </div>
   );
 }
 
