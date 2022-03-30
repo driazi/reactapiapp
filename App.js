819a97dbec6a54935ccb16809d16a2f5dcdc9a73
driazi <driaziran@gmail.com>
2022-03-30 10:42:13 +0430

    Add files via upload

--- App.js
+++ App.js
@@ -1,60 +1,76 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
-
 
 function App() {
 
-   const styleObj = {
+  const styleObj = {
     fontSize: 18,
     fontWeight: 'bold',
-    color: "#4a54f1",
-    textAlign: "center",
-    paddingTop: "20px",
+    color: "#ffffff",
+    textAlign: "left",
+    paddingTop: "15px",
+    margin: "20px"
 }
 
-  const imageUrl = "https://dummyjson.com/image/i/products/1/1.jpg";
-  const [img, setImg] = useState();
+const imageUrl = "https://dummyjson.com/image/i/products/1/1.jpg";
+const [img, setImg] = useState();
 
-  const fetchImage = async () => {
-    const res = await fetch(imageUrl);
-    const imageBlob = await res.blob();
-    const imageObjectURL = URL.createObjectURL(imageBlob);
-    setImg(imageObjectURL);
-  };
+const fetchImage = async () => {
+  const res = await fetch(imageUrl);
+  const imageBlob = await res.blob();
+  const imageObjectURL = URL.createObjectURL(imageBlob);
+  setImg(imageObjectURL);
+};
 
-  useEffect(() => {
-    fetchImage();
-  }, []);
+useEffect(() => {
+  fetchImage();
+}, []);
+
+// const thumbnailUrl = "https://dummyjson.com/image/i/products/1/thumbnail.jpg";
+// const [thumbnail, setThumbnail] = useState();
+
+//   const fetchThumbnail = async () => {
+//   const res = await fetch(thumbnailUrl);
+//   const imageBlob = await res.blob();
+//   const imageObjectURL = URL.createObjectURL(imageBlob);
+//   setThumbnail(imageObjectURL);
+// };
+
+// useEffect(() => {
+//   fetchThumbnail();
+// }, []);
 
 
-  const url = 'https://dummyjson.com/products/1';
-  const [data , setData] = useState({})
+const url='https://dummyjson.com/products/1';
+const [data, setData] = useState({})
 
+useEffect(() => {
+  fetch(url)
+  .then(res => res.json())
+  .then(res => setData(res))
+  .catch(error => console.log(error.message))
+} , [])
 
-  useEffect(()=>{
-    fetch(url)
-    .then(res => res.json())
-    .then(res => setData(res))
-    .catch(error=> console.log(error.message))
-  } , [])
 
 useEffect(()=>{
   console.log(data);
 } , [data])
 
+
   return (
-    <div className="App">
-      <div>
-        <p style={styleObj}>id : {data.id}</p>
-        <p style={styleObj}>title : {data.title}</p>
-        <p style={styleObj}>description : {data.description}</p>
-        <p style={styleObj}>category : {data.category}</p>
-        <>
-         <img src={img} alt="icons" />
-        </>
-    
+      <div className="App">
+        <div>
+          <p style={styleObj}>id : {data.id}</p>
+          <p style={styleObj}>title : {data.title}</p>
+          <p style={styleObj}>description : {data.description}</p>
+          <p style={styleObj}>category : {data.category}</p>
+          <p style={styleObj}>brand : {data.brand}</p>
+          <p style={styleObj}>price : {data.price}</p>
+          <>
+          <img src={img} alt="icons" />
+          </>
+          </div>
       </div>
-    </div>
   );
 }
 
